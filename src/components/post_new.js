e2941533ccdd427db438d400a86ae6b0ee99c729
Rhoulhac <greg.rhoulhac@gmail.com>
2018-03-01 10:46:04 -0600

    Add form labels and input fields

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
+
   render () {
     return (
       <div>
-        PostNew!
+        <form>
+          <Field
+          label="Title"
+            name='title'
+            component={this.renderField}
+          />
+          <Field
+            label="Categories"
+            name='categories'
+            component={this.renderField}
+          />
+          <Field
+            label="Post Content"
+            name='content'
+            component={this.renderField}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
