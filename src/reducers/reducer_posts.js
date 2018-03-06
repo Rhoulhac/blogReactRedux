5b4e02954b61ea8ccd7bf2b6534bac2b50909b7c
Rhoulhac <greg.rhoulhac@gmail.com>
2018-03-06 09:16:53 -0600

    Add component to show a post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,14 @@
-import { FETCH_POSTS } from '../actions';
 import _ from 'lodash';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
-  case FETCH_POSTS:
-    return _.mapKeys(action.payload.data, 'id')
-  default:
-    return state;
+    case FETCH_POST:
+    // ES6 key interpolation -> [action.payload.data.id]
+      return { ...state, [action.payload.data.id]: action.payload.data };
+    case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id');
+    default:
+      return state;
   }
 }
